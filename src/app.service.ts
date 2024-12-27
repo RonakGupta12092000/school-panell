b43f0571f379c643524faa82b7045e019bfcf234
ronaknetwallet <160584184+ronaknetwallet@users.noreply.github.com>
2024-12-27 22:04:20 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello GitHub!';
-  }
-}
+// @Injectable()
+// export class AppService {
+//   getHello(): string {
+//     return 'Hello GitHub!';
+//   }
+// }
